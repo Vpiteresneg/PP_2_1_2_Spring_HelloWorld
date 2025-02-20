821c9f4d3a14a260a1c660d3dae53658da6e93c7
Vpiteresneg <Shabalina_E@mail.ru>
2025-02-20 09:26:51 +0300

    Completed items 2,3,4,5,6 from the assignment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getName());
+
+        System.out.println("bean == bean2: " + (bean == bean2)); // true
+        System.out.println("bean3 == bean4: " + (bean3 == bean4)); // false
     }
 }
